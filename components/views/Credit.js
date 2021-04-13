19d0796431324e0b8dda00d9368d6e160a920e68
Shondanishea Edwards <shondaxavier2@gmail.com>
2021-04-12 19:09:05 -0500

    updating more info

--- components/views/Credit.js
+++ components/views/Credit.js
@@ -3,23 +3,23 @@ import html from "html-literal";
 export default (st) => html`
 
 <section id="credit">
-<h1>How to repair your credit score</h1>
+<h1>3 Ways to Improve your your Credit History</h1>
   <img src="https://trello-attachments.s3.amazonaws.com/6022d8763cabcd8163972e20/728x197/6671fade89960fec71d7613e80e4c684/27019190656_a3f40383aa_b.jpg" alt= Credit/>
 <div class="row">
 <div class="column">
-  <h2>Step One</h2>
-  <p id="rcorners1">
+  <h2>Check your credit report regularly</h2>
+  <p id="rcorners1"> When you comb through your credit reports, highlight your collections:those charged of accounts,those negative closed accounts,those repossession's, evictions, medical bill,etc.
   </p>
 </div>
 
 <div class="column">
-<h2>Step Two</h2>
-<p id="rcorners1">Check for errors</p>
+<h2>Check of Errors</h2>
+<p id="rcorners1">Credit repair is all about disputing inaccurate, incomplete, and unverifiable information- which is why debt validation is such an important step. Also check of errors in address and employment history, if incorrect have them removed. </p>
 </div>
 
 <div class="column">
-<h2>Step Three</h2>
-<p id="rcorners1">Dispute any errors on credit report</p>
+<h2>Rebuild your credit</h2>
+<p id="rcorners1">Pay bills on time. Lower your utilization of your credit card to less than 10%</p>
 </div>
 </section>
 `;
